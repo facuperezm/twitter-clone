8a7296c157be6809ed22017b4b26d6f3c6e5399f
facuperezm <facundoperezmontalvo@gmail.com>
2023-04-20 14:23:58 -0300

    rate limiter

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,8 @@ import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import {
   createTRPCRouter,
   privateProcedure,
@@ -16,6 +18,12 @@ const filteredUser = (user: User) => {
     profileImageUrl: user.profileImageUrl,
   };
 };
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -55,6 +63,13 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+      });
 
       const post = await ctx.prisma.posts.create({
         data: {
